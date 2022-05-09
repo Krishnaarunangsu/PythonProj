5bb0c03b44a0f473b025c46bb2a23757f85fc3e8
Krishnaarunangsu <arunangsutech@gmail.com>
2022-05-09 12:54:15 +0530

    Merge branch 'arun7' of https://github.com/Krishnaarunangsu/PythonProj into arun7
    # Conflicts:
    #	.gitignore

--- src/oop_tut/constructor_1_default_values.py
+++ src/oop_tut/constructor_1_default_values.py
@@ -1,19 +1,19 @@
 # Program to illustrate method overloading
 class Edpresso:
     """
-
+     Class to call a person
     """
 
     def __init__(self, id: int = 7):
         """
-
+        Initialization
         :param id:
         """
         self.id = id
 
     def hello(self, name=None):
         """
-
+        Calling a person
         :param name:
         :return:
         """
@@ -26,7 +26,7 @@ class Edpresso:
 # Create an instance
 obj = Edpresso()
 
-# Call the method wiyhout parameter
+# Call the method without parameter
 obj.hello()
 
 # Call the method with a parameter
