cec0e38c96d91c7105997ca908f8074ccd0ebdd3
arunangsucal <arunangsutech@gmail.com>
2025-12-10 17:52:46 +0530

    Updated the file

--- src/file_handling/file_read_1.py
+++ src/file_handling/file_read_1.py
@@ -26,7 +26,31 @@ class FileReader:
         finally:
             return self.raw_text
 
+    def read_line_file(self):
+        """
+        Read the file line by line
+        :return:
+        """
+        with open(self.file_name,'r') as file:
+            lines=file.readlines()
+
+            for line in lines:
+                print(line.strip()) # .strip() removes leading/trailing whitespace,
+                # including newline characters
+
+    def read_file_memory(self):
+        """
+
+        :return:
+        """
+        with open(self.file_name, 'r') as file:
+            for line in file:
+                print(line.strip())
+
+
 if __name__=="__main__":
-    file_name="C:\\Arunangsu\\Test_Data\\file_2.txt"
+    file_name="C:\\Arunangsu\\Test_Data\\file_1.txt"
     file_reader=FileReader(file_name)
     raw_data=file_reader.read_file()
+    file_reader.read_line_file()
+    file_reader.read_file_memory()
